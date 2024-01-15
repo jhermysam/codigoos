cd64eb9bbc31d49667f65f3d0cf291f0277c1340
jhermysam <jhermysamuel2@gmail.com>
2024-01-15 01:48:07 -0500

    a2

---
+++ propiedades.py
@@ -0,0 +1,26 @@
+class persona:
+    def __init__(self,nombre,edad):
+        self.__nombre = nombre
+        self._edad = edad 
+
+    @property
+    def nombre(self):
+        return self.__nombre
+    
+    @nombre.setter
+    def nombre(self, new_nombre):
+        self.__nombre= new_nombre
+
+    @nombre.deleter
+    def nombre(self):
+        del self.__nombre
+    
+dalto = persona("Lucas",21)
+
+nombre = dalto.nombre 
+print(nombre)
+
+dalto.nombre = "Pepe"
+
+nombre = dalto.nombre
+print(nombre)
